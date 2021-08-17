b7cbca263f5f325eaff2a896c2c8a16c1fa93544
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-08-16 17:50:46 -0700

    get Pizza info API fetch comments by ID  function

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -133,7 +133,7 @@ function handleNewCommentSubmit(event) {
     .catch(err => {
       console.log(err);
     });
-
+}
 
 function handleNewReplySubmit(event) {
   event.preventDefault();
@@ -153,13 +153,32 @@ function handleNewReplySubmit(event) {
 
   const formData = { writtenBy, replyBody };
 
-  
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 $backBtn.addEventListener('click', function() {
   window.history.back();
 });
-
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
 
